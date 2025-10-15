82d4554bf067fa3c04752b851c965055c0ef956d
Alice Alysia <alice@alysia.au>
2025-10-15 20:39:00 +1100

    default settings for alysios

---
+++ monitors.nix
@@ -0,0 +1,17 @@
+{...}:{
+  programs.niri.settings.outputs = {
+    "Virtual-1" = {
+      mode = {
+        width = 1920;
+        height = 1080;
+      };
+    };
+    "Dell Inc. DELL S2422HG 7DCFK83" = {
+      mode = {
+        width = 1920;
+        height = 1080;
+        refresh = 164.997;
+      };
+    };
+  };
+}
