133c45687a2a2ce38e8be750f4befbd82807d91a
Alice Alysia <alice@alysia.au>
2025-10-20 00:23:48 +1100

    updates

--- flake.nix
+++ flake.nix
@@ -12,19 +12,9 @@
       url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    dgop = {
-      url = "github:AvengeMedia/dgop";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    dms-cli = {
-      url = "github:AvengeMedia/danklinux";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     dankMaterialShell = {
       url = "github:AvengeMedia/DankMaterialShell";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.dgop.follows = "nixpkgs";
-      inputs.dms-cli.follows = "dms-cli";
     };
   };
 
